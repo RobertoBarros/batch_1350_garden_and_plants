4663725ad3ec5332f2ac50f8312747b14310d187
Roberto Barros <roberto.f.barros@gmail.com>
2023-08-23 18:17:27 -0300

    livecode

---
+++ db/seeds.rb
@@ -0,0 +1,2 @@
+Garden.create!(name: 'My Super Garden', image_url: 'https://plus.unsplash.com/premium_photo-1675039871605-eb156cc0575d')
+Garden.create!(name: 'Other Garden', image_url: 'https://images.unsplash.com/photo-1585320806297-9794b3e4eeae')
